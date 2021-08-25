53a67f2bd854418cd4c2a55f9e7880cb755844b3
editid0 <66117959+editid0@users.noreply.github.com>
2021-08-25 15:25:10 +0200

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,1 +1,1 @@
-echo "python-init=$(curl https://raw.githubusercontent.com/editid0/python-init/main/linux/main.sh | bash)" >> ~/.bashrc
+echo $"python-init=\"curl https://raw.githubusercontent.com/editid0/python-init/main/linux/main.sh | bash\"" >> ~/.bashrc
